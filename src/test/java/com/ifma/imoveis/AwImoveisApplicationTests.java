599179aa0f92ef23c4278a2cf3ba095e2da4b550
josemarmendes <josemarmendes17@gmail.com>
2017-09-20 09:41:24 -0300

    corrigindo nome de pacotes e classes

--- src/test/java/com/ifma/imoveis/AwImoveisApplicationTests.java
+++ src/test/java/com/ifma/imoveis/AwImoveisApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ifma.carros;
+package com.ifma.imoveis;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AwCarrosApplicationTests {
+public class AwImoveisApplicationTests {
 
 	@Test
 	public void contextLoads() {
